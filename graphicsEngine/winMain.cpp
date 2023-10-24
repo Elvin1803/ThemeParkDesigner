ea568bd6c8b9449dd068bd5dbf6e83489321ce56
Elvin1803 <sisavath2003@gmail.com>
2023-10-24 21:35:43 +0200

    Program stops on windowd closed

--- graphicsEngine/winMain.cpp
+++ graphicsEngine/winMain.cpp
@@ -1,4 +1,17 @@
+#include <iostream>
 #include <Windows.h>
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+    switch (msg)
+    {
+    case WM_CLOSE:
+        PostQuitMessage(0);
+        break;
+    }
+
+    return DefWindowProc(hWnd, msg, wParam, lParam);
+}
 
 int CALLBACK WinMain(
     HINSTANCE hInstance,
@@ -12,7 +25,7 @@ int CALLBACK WinMain(
     WNDCLASSEX wc = {0};
     wc.cbSize = sizeof(wc);
     wc.style = CS_OWNDC;
-    wc.lpfnWndProc = DefWindowProc;
+    wc.lpfnWndProc = WndProc;
     wc.cbClsExtra = 0;
     wc.cbWndExtra = 0;
     wc.hInstance = hInstance;
@@ -22,6 +35,7 @@ int CALLBACK WinMain(
     wc.lpszMenuName = nullptr;
     wc.lpszClassName = pClassName;
     wc.hIconSm = nullptr;
+
     RegisterClassEx(&wc);
 
     // create window instance
@@ -29,12 +43,26 @@ int CALLBACK WinMain(
         0, pClassName,
         "Theme Park Designer",
         WS_CAPTION | WS_MINIMIZEBOX | WS_MAXIMIZEBOX | WS_SYSMENU,
-        200, 200, 1280, 720,
+        200, 200,
+        1280, 720,
         nullptr, nullptr, hInstance, nullptr
     );
 
     ShowWindow(hWnd, SW_SHOW);
-    while (true);
 
-    return 0;
+    // messages
+    MSG msg;
+    BOOL gResult;
+    while ((gResult = GetMessage(&msg, nullptr, 0, 0)) > 0)
+    {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+
+    if (gResult == -1)
+    {
+        return -1;
+    }
+
+    return msg.wParam;
 }
