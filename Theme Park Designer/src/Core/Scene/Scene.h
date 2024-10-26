2b72cb967d145ee9f32b942f40f667f5ccfe0d1b
Elvin1803 <sisavath2003@gmail.com>
2024-10-26 17:37:12 +0200

    Added scene class

---
+++ Theme Park Designer/src/Core/Scene/Scene.h
@@ -0,0 +1,17 @@
+#ifndef SCENE_H
+#define SCENE_H
+
+#include <entt/entt.hpp>
+
+namespace TPD
+{
+
+    class Scene
+    {
+    private:
+        entt::registry m_registry;
+    };
+
+}
+
+#endif /* SCENE_H */
